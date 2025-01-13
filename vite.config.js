a8252f1ba12a08d59ce89ea297c8cc2edd8c6494
LuzBoger <arthurbrouard2003@gmail.com>
2025-01-13 22:55:28 +0100

    patch système de routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  // Ajoutez cette configuration :
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: '/',
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
+  optimizeDeps: {
+    include: ['react/jsx-runtime'],
+  },
+  // Utilisez ce paramètre pour corriger le routage
+  esbuild: true,
+
 })
